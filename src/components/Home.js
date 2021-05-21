fa92b6ad7d7336e1768841b8d1dc6116b76b59fc
Prithviraj <prithvirajmahamulkar100@gmail.com>
2021-05-21 23:45:39 +0530

    Sections for tesla

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,10 +9,56 @@ export const Container = styled.div`
 function Home() {
   return (
     <Container>
-      <Section />
-      <Section />
-      <Section />
-      <Section />
+      <Section
+        title="Model S"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-y.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model 3"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-3.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model X"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-x.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Lowest Cost Solar Panels in Phaltan"
+        description="Money-back guarantee"
+        backgroundImg="solar-panel.jpg"
+        leftBtnText="Order now"
+        rightBtnText="Learn more"
+      />
+
+      <Section
+        title="Solar for New Roofs"
+        description="Solar Roof Costs Less Than a New Roof Plus Solar Panels"
+        backgroundImg="solar-roof.jpg"
+        leftBtnText="Order now"
+        rightBtnText="Learn more"
+      />
+
+      <Section
+        title="Accessiories"
+        description=""
+        backgroundImg="accessories.jpg"
+        leftBtnText="Shop now"
+      />
     </Container>
   );
 }
--- src/components/Section.js
+++ src/components/Section.js
@@ -12,20 +12,27 @@ export const Wrap = styled.div`
   flex-direction: column;
   justify-content: space-between; // Vertical
   align-items: center; // horizantal
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
 `;
 
-function Section() {
+function Section({
+  title,
+  description,
+  leftBtnText,
+  rightBtnText,
+  backgroundImg,
+}) {
   return (
-    <Wrap>
+    <Wrap bgImage={backgroundImg}>
       <ItemText>
-        <h1>Model S</h1>
-        <p>Order Online For Touchless Delivery.</p>
+        <h1>{title}</h1>
+        <p>{description}</p>
       </ItemText>
 
       <Button>
         <ButtonGroup>
-          <LeftButton>Custom Order</LeftButton>
-          <RightButton>Existing Inventory</RightButton>
+          <LeftButton>{leftBtnText}</LeftButton>
+          {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
         </ButtonGroup>
 
         <DownArrow src="/images/down-arrow.svg" />
